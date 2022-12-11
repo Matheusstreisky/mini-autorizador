db6519472055fea83c36994299aa0376d3759091
Matheus Streisky <streiskymatheus@gmail.com>
2022-12-11 14:45:16 -0300

    Adicionada funcionalidade de obter saldo do cartão

--- src/main/java/com/streisky/miniautorizador/controller/CartaoController.java
+++ src/main/java/com/streisky/miniautorizador/controller/CartaoController.java
@@ -1,10 +1,13 @@
 package com.streisky.miniautorizador.controller;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,15 @@ public class CartaoController {
 		} catch (DataIntegrityViolationException e) {
 			return ResponseEntity.unprocessableEntity().body(cartaoForm);
 		}
+	}
+	
+	@GetMapping("/{numeroCartao}")
+	public ResponseEntity<String> obterSaldoCartao(@PathVariable String numeroCartao) {
+		Optional<Cartao> optional = cartaoRepository.findByNumeroCartao(numeroCartao);
+		
+		return optional
+				.map(o -> ResponseEntity.ok().body(o.getSaldo().toString()))
+				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 
 }
